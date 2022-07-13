efb0e1a16126078fd5091ea9fb9498acb75774d2
David <benjamindavid023@gmail.com>
2022-07-13 18:17:27 +0530

    added new changes

--- Assessment1-A/Solution.cs
+++ Assessment1-A/Solution.cs
@@ -18,23 +18,23 @@ namespace Assessment1
             {
                 case "1":
                     Console.WriteLine("you have entered one");
-                    GetQuestion1();
+                    GetSumofOddIndex();
                     break;
                 case "2":
                     Console.WriteLine("you have entered two");
-                    GetQuestion2Version2();
+                    FindMissingNumber();
                     break;
                 case "3":
                     Console.WriteLine("you have entered three");
-                    GetQuestion3();
+                    FindTargetIndex();
                     break;
                 case "4":
                     Console.WriteLine("you have entered four");
-                    GetQuestion4();
+                    GetMinAndMax();
                     break;
                 case "5":
                     Console.WriteLine("you have entered five");
-                    GetQuestion5();
+                    GetPrimeAndNonPrime();
                     break;
                 default:
                     Console.WriteLine("Please enter the number 1 to 5");
@@ -42,23 +42,27 @@ namespace Assessment1
                     break;
             }
         }
-        public static void GetQuestion1()
+        public static void GetSumofOddIndex()
         {
+            // Input int only
             try
             {
                 Console.WriteLine("Q1.Add the sequence of number in odd index\nEnter the sequence of number");
 
-                String userInput = Console.ReadLine();
-                if (userInput.Length == 0 || userInput ==null)
+                string userInput = Console.ReadLine();
+                
+                int[] intArray = IntToArray(Convert.ToInt32(userInput));
+                int result = 0;
+
+                if (intArray.Length == 0 || intArray == null)
                 {
                     Console.WriteLine("Please enter the valid input...");
-                    GetQuestion1();
+                    GetSumofOddIndex();
                 }
-                
-                int result = 0;
-                for (int i = 0; i < userInput.Length; i += 2)
+
+                for (int i = 0; i < intArray.Length; i += 2)
                 {
-                    int temp = Convert.ToInt32(userInput[i].ToString());
+                    int temp = Convert.ToInt32(intArray[i].ToString());
                     result += temp;
                 }
                 Console.WriteLine("Input: {0} \nOutput: {1}", userInput, result);
@@ -72,54 +76,41 @@ namespace Assessment1
 
         }
 
-        public static void GetQuestion2()
+        public static void FindMissingNumber()
         {
             try
             {
-                Console.WriteLine("Q2.Find the missing number In the array.\nEnter the Array:\n(Example: 1 2 3 5)");
-                
-                int[] array = Array.ConvertAll(Console.ReadLine().Trim().Split(' '), Convert.ToInt32);
+                Console.WriteLine("Q2.Find the missing number In the array.\nEnter the array length");
+                var arrayLength = Convert.ToInt32(Console.ReadLine());
 
-                int n = array.Length;
+                Console.WriteLine("Enter the Array:\n(Example: 1 2 3 5)");
 
-                int total = (n + 1) * (n + 2) / 2;
-                for (int i = 0; i < array.Length; i++)
+                //int[] array = Array.ConvertAll(Console.ReadLine().Trim().Split(' '), Convert.ToInt32);
+
+                int[] userInput = new int[arrayLength];
+
+                for(int i = 0; i < arrayLength; i++)
                 {
-                    total -= array[i];
+                    userInput[i] = Convert.ToInt32(Console.ReadLine());
                 }
-                Console.WriteLine(total);
 
-                Execute();
-            }catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-        }
-
-        public static void GetQuestion2Version2()
-        {
-            try
-            {
-                Console.WriteLine("Q2.Find the missing number In the array.\nEnter the Array:\n(Example: 1 2 3 5)");
-
-                int[] array = Array.ConvertAll(Console.ReadLine().Trim().Split(' '), Convert.ToInt32);
-
-                if (array.Length == 0 || array == null)
+                if (userInput.Length == 0 || userInput == null)
                 {
                     Console.WriteLine("Please enter the valid input...");
-                    GetQuestion2Version2();
+                    FindMissingNumber();
                 }
                 
                 var missingNumbers = new List<int>();
 
-                for (int i = array.Min(); i <= array.Max(); i++)
+                for (int i = userInput.Min(); i <= userInput.Max(); i++)
                 {
-                    if (!array.Contains(i))
+                    if (!userInput.Contains(i))
                     {
                         missingNumbers.Add(i);
                     }
                 }
-                
+
+                Console.WriteLine("User Input: [{0} ]", String.Join(", ", userInput));
                 Console.WriteLine(String.Format("Missing Numbers : [ {0} ]", String.Join(", ", missingNumbers)));
                 if (missingNumbers.Count == 0)
                 {
@@ -133,24 +124,33 @@ namespace Assessment1
             }
         }
 
-        public static void GetQuestion3()
+        public static void FindTargetIndex()
         {
             try
             {
                 Console.WriteLine("Q3.Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.  ");
+                Console.WriteLine("Enter the array length");
+                
+                var arrayLength = Convert.ToInt32(Console.ReadLine());
+                
                 Console.WriteLine("Enter array input: \n(Example: 9 3 5 2 32 22)");
                 
-                int[] array = Array.ConvertAll(Console.ReadLine().Trim().Split(' '), Convert.ToInt32);
+                int[] userInput = new int[arrayLength];
+                for (int i = 0; i < arrayLength; i++)
+                {
+                    userInput[i] = Convert.ToInt32(Console.ReadLine());
+                }
 
+                Console.WriteLine("Given input: [ {0}]", String.Join(",", userInput));
                 Console.WriteLine("Enter target");
                 int target = Convert.ToInt32(Console.ReadLine());
 
                 
-                for(int i = 0; i < array.Length; i++)
+                for(int i = 0; i < userInput.Length; i++)
                 {
-                    for(int j = i+1; j< array.Length; j++)
+                    for(int j = i+1; j< userInput.Length; j++)
                     {
-                        if(target == array[j] + array[i])
+                        if(target == userInput[j] + userInput[i])
                         {
                             Console.WriteLine("[ {0}, {1} ]", i, j);
                         }
@@ -165,16 +165,24 @@ namespace Assessment1
             }
         }
 
-        public static void GetQuestion4()
+        public static void GetMinAndMax()
         {
             try
             {
                 Console.WriteLine("Q4. Find the minimum and maximum elements in array");
+                Console.WriteLine("Enter the array length");
+
+                var arrayLength = Convert.ToInt32(Console.ReadLine());
+
                 Console.WriteLine("Enter the Array:\n(Example: 9 3 5 2 32 22)");
+                int[] userInput = new int[arrayLength];
+                
+                for (int i = 0; i < arrayLength; i++)
+                {
+                    userInput[i] = Convert.ToInt32(Console.ReadLine());
+                }
 
-                int[] array = Array.ConvertAll(Console.ReadLine().Trim().Split(' '), Convert.ToInt32);
-
-                Console.WriteLine("Min: {0} \nMax: {1}", array.Min(), array.Max());
+                Console.WriteLine("Min: {0} \nMax: {1}", userInput.Min(), userInput.Max());
                 
                 Execute();
             }
@@ -184,7 +192,7 @@ namespace Assessment1
             }
         }
 
-        public static void GetQuestion5()
+        public static void GetPrimeAndNonPrime()
         {
             Console.WriteLine("Q5.Separate the Prime and Non Prime number from the given array");
             Console.WriteLine("Enter array1 input: \n (Example: 9 3 5 2 32 22)");
@@ -221,6 +229,20 @@ namespace Assessment1
             Console.WriteLine(String.Format("Non Prime Number is : [ {0} ]", String.Join(", ", nonPrime)));
 
             Execute();
+        }
+
+        public static int[] IntToArray(int n)
+        {
+            if (n == 0) return new int[1] { 0 };
+
+            var digits = new List<int>();
+
+            for (; n != 0; n /= 10)
+                digits.Add(n % 10);
+
+            var arr = digits.ToArray();
+            Array.Reverse(arr);
+            return arr;
         }
 
     }
